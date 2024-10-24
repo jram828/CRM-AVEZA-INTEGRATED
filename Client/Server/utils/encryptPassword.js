1a4d86c5ecc163cd52c95d4b4eb826f49ac54e43
jram828 <128335805+jram828@users.noreply.github.com>
2024-10-23 19:49:29 -0500

    include await

--- Client/Server/utils/encryptPassword.js
+++ Client/Server/utils/encryptPassword.js
@@ -6,8 +6,8 @@ config(); // Cargar variables de entorno desde el archivo .env
 const { SALT_BCRYPT } = process.env;
  console.log('SALT_BCRYPT:', SALT_BCRYPT);
 export const encryptPassword = async (password) => {
-  const salt = bcrypt.genSalt(12);
-  return bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(12);
+  return await bcrypt.hash(password, salt);
 };
 
 export const verifyPassword = async (bodyPassword, userPassword) => {

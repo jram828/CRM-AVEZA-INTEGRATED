3f6d6dd108edd9667fae63c42823f876e9c5c1fb
jram828 <128335805+jram828@users.noreply.github.com>
2024-11-13 23:57:16 -0500

    console log req.file

--- Client/Server/handlers/uploadHandlers.js
+++ Client/Server/handlers/uploadHandlers.js
@@ -1,7 +1,7 @@
 import { authenticateGoogle, deleteFile, uploadToGoogleDrive } from "../controllers/upload/upload.js";
 
 export const uploadHandler = async (req, res, next) => {
-    console.log("FILE",req);
+    console.log("FILE",req.file);
   try {
     if (!req.file) {
       res.status(400).send("No file uploaded.");

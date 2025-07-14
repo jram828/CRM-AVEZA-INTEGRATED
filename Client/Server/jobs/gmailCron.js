0eddda10255f03cd77c765c0c646b91c8913c2ac
jram828 <128335805+jram828@users.noreply.github.com>
2025-07-14 13:38:23 -0500

    fix(Prospectos): :bug: Console log verificar cron

--- Client/Server/jobs/gmailCron.js
+++ Client/Server/jobs/gmailCron.js
@@ -1,6 +1,8 @@
 import cron from 'node-cron';
 import { buscarCorreos } from '../utils/getDataFromEmail.js'; // ← Ajusta la ruta según donde esté tu lógica actual
 
+console.log('📦 gmailCron.js cargado correctamente');
+
 // Ejecutar cada 12 minutos
 cron.schedule('*/12 * * * *', () => {
   console.log('⏰ Ejecutando tarea automática de lectura de correos');

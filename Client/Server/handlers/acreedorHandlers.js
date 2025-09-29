c3785d692a19c4ced876f33149301c8a2a0829ab
jram828 <128335805+jram828@users.noreply.github.com>
2025-09-28 20:58:14 -0500

    Fix nombre ciudad crear acreedor

--- Client/Server/handlers/acreedorHandlers.js
+++ Client/Server/handlers/acreedorHandlers.js
@@ -24,7 +24,7 @@ const postAcreedorHandler = async (req, res) => {
     email,
     nombre,
     direccion,
-    nombre_ciudad,
+    ciudad,
     telefono,
     idProspecto,
   } = req.body;
@@ -34,7 +34,7 @@ const postAcreedorHandler = async (req, res) => {
     email,
     nombre,
     direccion,
-    nombre_ciudad,
+    ciudad,
     telefono,
     idProspecto,
   });
@@ -45,7 +45,7 @@ const postAcreedorHandler = async (req, res) => {
       email,
       nombre,
       direccion,
-      nombre_ciudad,
+      ciudad,
       telefono,
       idProspecto
     );

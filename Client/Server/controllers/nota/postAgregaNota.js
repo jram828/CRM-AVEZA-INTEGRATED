d770785e98e551d4162e7dcc74480304f13d20a2
jram828 <128335805+jram828@users.noreply.github.com>
2026-02-22 22:46:30 -0500

    Fix, associate note to client

--- Client/Server/controllers/nota/postAgregaNota.js
+++ Client/Server/controllers/nota/postAgregaNota.js
@@ -1,7 +1,7 @@
 import { models } from "../../DB.js";
 
-const { Nota, Prospecto} = models;
-const createNota = async (idProspecto, descripcion) => {
+const { Nota, Prospecto, Cliente } = models;
+const createNota = async (idProspecto, descripcion, source) => {
 
 
   const newNota = await Nota.create({
@@ -10,11 +10,17 @@ const createNota = async (idProspecto, descripcion) => {
 
  console.log("Nueva nota creada en la base de datos:", newNota);
 
+ if( source === "prospecto") {
   const prospecto = await Prospecto.findByPk(idProspecto);
   if (!prospecto) throw Error("Prospecto no encontrado");
 
   await prospecto.addNota(newNota);
+ } else if (source === "cliente") {
+  const cliente = await Cliente.findByPk(idProspecto);
+  if (!cliente) throw Error("Cliente no encontrado");
 
+  await cliente.addNota(newNota);
+ }
   return newNota;
 };
 
